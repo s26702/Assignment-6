ddea6e7ebd3a7bb4704a3eb0e199d0a9ffd30394
s254961-pixel <s254961@dtu.dk>
2026-03-20 11:34:53 +0100

    Update BoardFactory

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/BoardFactory.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/BoardFactory.java
@@ -43,23 +43,37 @@ public class BoardFactory {
      * @param name the given name board
      * @return the new board corresponding to that name
      */
+
+    /**
+     * * This method implements a factory pattern to return different board
+     *   configurations. If the name is unknown or null, a default "Simple"
+     *   board is returned to ensure the game can always start (defensive programming).
+     * @param name the name of the board to be created
+     * @return the new board corresponding to the given name
+     */
     public Board createBoard(String name) {
         // TODO A6b: Implement this method properly as described in Assignment 6b.
         //     Dependent on the provided name, create a board accordingly and
         //     return it. In case the name is null, some default board should
         //     be returned (defensive programming).
 
-            if (name == null || name.equals("Simple")) {
-                return createDefaultBoard();
-            } else if (name.equals("Advanced")) {
-                return createAdvancedBoard();
-            } else {
-                return createDefaultBoard();
-            }
+        if (name == null || name.equals("Simple")) {
+            return createDefaultBoard();
+        } else if (name.equals("Advanced")) {
+            return createAdvancedBoard();
+        } else {
+            return createDefaultBoard();
         }
+    }
 
-        private Board createDefaultBoard() {
-            Board board = new Board(8, 8, "Simple");
+
+    /**
+     * Creates a basic 8x8 board named "Simple" with a few walls and conveyor belts.
+     *
+     * @return a simple Board configuration
+     */
+    private Board createDefaultBoard() {
+        Board board = new Board(8, 8, "Simple");
 
         // add some walls, actions and checkpoints to some spaces
         Space space = board.getSpace(0,0);
@@ -93,6 +107,16 @@ public class BoardFactory {
 
         return board;
     }
+
+
+    /**
+     * Creates a more complex 10x10 board named "Advanced".
+     *
+     * This board contains more advanced features such as additional walls
+     * and conveyor belts facing different directions to increase difficulty.
+     *
+     * @return an advanced Board configuration
+     */
     private Board createAdvancedBoard() {
         Board board = new Board(10, 10, "Advanced");
 
@@ -108,15 +132,6 @@ public class BoardFactory {
         belt2.setHeading(Heading.SOUTH);
         board.getSpace(4, 1).getActions().add(belt2);
 
-        Checkpoint checkpoint1 = new Checkpoint(1);
-        board.getSpace(3, 3).getActions().add(checkpoint1);
-
-        Checkpoint checkpoint2 = new Checkpoint(2);
-        board.getSpace(6, 6).getActions().add(checkpoint2);
-
-        Checkpoint checkpoint3 = new Checkpoint(3);
-        board.getSpace(9, 9).getActions().add(checkpoint3);
-
         return board;
     }
 
@@ -128,6 +143,13 @@ public class BoardFactory {
     //     here has a proper JavaDoc documentation.
     //
 
+    /**
+     * Returns a list of all available board names that this factory can create.
+     * This method is used by the AppController to populate the board selection
+     * dialog, allowing the user to choose between different game arenas.
+     *
+     * @return a List of Strings containing all available board names
+     */
     public java.util.List<String> getBoardNames() {
         java.util.List<String> names = new java.util.ArrayList<>();
         names.add("Simple");
