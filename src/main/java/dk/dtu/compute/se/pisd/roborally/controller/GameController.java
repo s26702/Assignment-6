e8f22a9497d86ab2b3a743a4f5aa0274a3973c60
s26702 <niklashaislund@gmail.com>
2026-04-10 10:41:25 +0200

    og

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
@@ -303,7 +303,9 @@ public class GameController {
     }
 
     public void moveForward(Player player, Heading heading) {
+
         Space next = board.getNeighbour(player.getSpace(), heading);
+        if (next == null || )
 
         for (int i = 0; i < board.getPlayersNumber(); i++) {
             Player other = board.getPlayer(i);
