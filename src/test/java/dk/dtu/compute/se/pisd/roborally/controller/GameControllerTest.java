7742a502b17193bbc62e9eaa54cc26084e79ab48
s26702 <niklashaislund@gmail.com>
2026-04-11 10:11:53 +0200

    h,

--- src/test/java/dk/dtu/compute/se/pisd/roborally/controller/GameControllerTest.java
+++ src/test/java/dk/dtu/compute/se/pisd/roborally/controller/GameControllerTest.java
@@ -3,10 +3,10 @@ package dk.dtu.compute.se.pisd.roborally.controller;
 import dk.dtu.compute.se.pisd.roborally.model.Board;
 import dk.dtu.compute.se.pisd.roborally.model.Heading;
 import dk.dtu.compute.se.pisd.roborally.model.Player;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
 import dk.dtu.compute.se.pisd.roborally.model.Phase;
 
 
