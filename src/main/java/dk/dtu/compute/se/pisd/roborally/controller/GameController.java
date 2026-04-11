36449b1cc95a80fafa51879737fb009158dee626
s26702 <niklashaislund@gmail.com>
2026-04-11 09:38:07 +0200

    ,

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
@@ -329,7 +329,6 @@ public class GameController {
         if(player == null || player.getSpace() == null || heading == null) {
             return;
         }
-
         Space next = board.getNeighbour(player.getSpace(), heading);
         if(next == null || board.getNieghborwall(player.getSpace(), heading)) {
             return;
