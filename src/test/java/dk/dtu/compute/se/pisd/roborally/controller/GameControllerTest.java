513d027fec02dc22aa372d5aaeb7b31232ee87ad
s26702 <niklashaislund@gmail.com>
2026-04-11 10:17:12 +0200

    Update GameControllerTest.java

--- src/test/java/dk/dtu/compute/se/pisd/roborally/controller/GameControllerTest.java
+++ src/test/java/dk/dtu/compute/se/pisd/roborally/controller/GameControllerTest.java
@@ -1,5 +1,4 @@
 package dk.dtu.compute.se.pisd.roborally.controller;
-
 import dk.dtu.compute.se.pisd.roborally.model.Board;
 import dk.dtu.compute.se.pisd.roborally.model.Heading;
 import dk.dtu.compute.se.pisd.roborally.model.Player;
